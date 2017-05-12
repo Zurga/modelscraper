0f4934aef1031fe2c5a02e724b67a9f4ef2334e4
Jim <jim.lemmers@gmail.com>
2017-05-12 17:04:07 +0200

    added a check to the start up sequence to check whether the model is concisely built and the functions are spelt correctly

--- modelscraper/dispatcher.py
+++ modelscraper/dispatcher.py
@@ -1,8 +1,8 @@
 from . import workers
 from multiprocessing import Queue
-#from queue import Queue
 from . import databases
 from collections import defaultdict
+import sys
 
 
 class Dispatcher:
@@ -14,7 +14,6 @@ class Dispatcher:
         self.store_threads = []
 
     def run(self):
-#        try:
         for scraper in self.scrapers.values():
             scraper.start()
             print('started', scraper.name)
@@ -31,8 +30,6 @@ class Dispatcher:
                 url = input('url: ')
                 scraper.add_source(url)
 
-#        except KeyboardInterrupt:
-
         # TODO fix that the seen urls go to the databases.
         print('joined')
 
@@ -43,18 +40,42 @@ class Dispatcher:
             models = [models]
 
         db_threads = defaultdict(list)
+
         for model in models:
             for run in model.runs:
-                for temp in run.templates:
-                    if temp.db_type:
-                        db_threads[temp.db_type].append(temp)
+                for template in run.templates:
+                    if template.db_type:
+                        db_threads[template.db_type].append(template)
+
+                    self._check_functions(template, run)
 
         for model in models:
             scraper = workers.ScrapeWorker(model, store_q=self.store_q)
             self.scrapers[scraper.name] = scraper
 
         for thread, templates in db_threads.items():
-            # TODO start the right database threads
             print('starting', thread)
             store_thread = databases._threads[thread](in_q=self.store_q)
             store_thread.start()
+
+    def _check_functions(self, template, run):
+        error_string = "One of these functions: {} is not implemented in {}."
+        not_implemented = []
+
+        for attr in template.attrs.values():
+            for func in attr.func:
+                if not getattr(run.parser, func, False):
+                    not_implemented.append(func)
+
+        if not_implemented:
+            raise Exception(error_string.format(str(not_implemented),
+                                                run.parser.__class__.__name__))
+
+    def print_progress(progress):
+        sys.stdout.write('\033[2J\033[H')  # clears the screen
+        for scraper, percent, run in progress:
+            bar = ('=' * int(percent * 20)).ljust(20)
+            percent = int(percent * 100)
+            sys.stdout.write("%s %s [%s] %s%%\n" % (scraper, run,
+                                                    bar, percent))
+            sys.stdout.flush()
