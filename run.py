0918506bdc34b357263d17b8224af791b7c663b9
Jim <jim.lemmers@gmail.com>
2017-11-29 20:03:29 +0100

    added a run script which will kickstart the scrape model specified

--- run.py
+++ run.py
@@ -2,8 +2,17 @@
 
 import importlib
 import os
+import pprint
 
 import click
+
+from modelscraper.components import ScrapeModel
+from modelscraper.dispatcher import Dispatcher
+
+
+if 'banner.txt' in os.listdir():
+    with open('banner.txt') as fle:
+        print(fle.read())
 
 available_models = [f[:-3] for f in os.listdir('scrape_models')
                   if f.endswith('.py') and not f.startswith('__')]
@@ -11,16 +20,19 @@ available_models = [f[:-3] for f in os.listdir('scrape_models')
 @click.argument('model', nargs=-1)
 @click.option('--dummy', default=False, help='Whether to do a dummy run')
 def main(model, dummy):
+    print(dummy)
+    if len(model) == 1:
+        model = model[0]
     dispatcher = Dispatcher()
     if model not in available_models:
         print('Model', model, 'is not in the folder "scrape_models".')
-        os.exit(1)
-
-    scrape_model = importlib.import_module(f'scrape_models.{model}')
-    for variable in vars(scrape_model):
-        if type(variable) == ScrapeModel:
-            dispatcher.add_scraper(variable, dummy=dummy)
-
+        print('These models are available:')
+        pprint.pprint(available_models, compact=True)
+        return
+    imported = vars(importlib.import_module(f'scrape_models.{model}')).values()
+    scrape_models = [model for model in imported
+                     if type(model) == ScrapeModel]
+    dispatcher.add_scraper(scrape_models, dummy=dummy)
     dispatcher.run()
 
 if __name__ == '__main__':
