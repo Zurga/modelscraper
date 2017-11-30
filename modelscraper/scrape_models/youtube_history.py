466c20e9bd77682f47c14ed675d565c4ed73a6ad
Jim <jim.lemmers@gmail.com>
2017-11-30 01:15:33 +0100

    moved the old scrape models to the new folder

--- modelscraper/scrape_models/youtube_history.py
+++
@@ -1,9 +0,0 @@
-from dispatcher import Dispatcher
-import re
-import models
-from functions.store_functions import *
-from functions.parse_functions import *
-from pymongo import MongoClient
-
-youtube = ScrapeModel(name='youtube_history', domain='youtube.com', phases=[
-    Phase(getters=[models.Getter(url='sl
