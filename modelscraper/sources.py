5dbf0965748eeb0c137c00cf7c91a9d9310e594a
Jim <jim.lemmers@gmail.com>
2017-12-03 00:36:41 +0100

    refactored the run method in every subclass to the retrieve method. The "run" method is now implemented in the base class and works with a Thread Event to check whether it should keep running. It also keeps track of how long each retrieval of a source took.

--- modelscraper/sources.py
+++ modelscraper/sources.py
@@ -6,18 +6,35 @@ import subprocess
 
 
 class BaseSourceWorker(Thread):
-    def __init__(self, parent=None, id=0):
+    def __init__(self, parent=None, id=0, stop_event=None):
         super().__init__()
         if parent:
             self.id  = id
             self.in_q = parent.source_q
             self.out_q = parent.parse_q
             self.parent = parent
+            self.stop_event = stop_event
+        self.mean = 0
+        self.total_time = 0
+        self.visited = 0
 
     def __call__(self, **kwargs):
-        return self.__class__(**{**kwargs, **self.__dict__})
+        return self.__class__(**kwargs, **self.inits)
 
     def run(self):
+        print('started')
+        while not self.stop_event.wait(0):
+            start = time.time()
+            source = self.retrieve(self.in_q.get())
+            if source:
+                self.out_q.put(source)
+            self.visited += 1
+            self.total_time += time.time() - start
+            self.mean = self.total_time / self.visited
+            self.in_q.task_done()
+        print('Done')
+
+    def retrieve(self):
         raise NotImplementedError
 
 
@@ -36,62 +53,46 @@ class WebSource(BaseSourceWorker):
         self.time_out = time_out
         self.times = []
         self.to_parse = self.parent.to_parse
-        self.mean = 0
-        self.total_time = 0
-        self.visited = 0
         self.user_agent = self.parent.model.user_agent
 
         self.connection_errors = []
 
-    def run(self):
-        while True:
-            source = self.in_q.get()
+    def retrieve(self, source):
+        headers = {'User-Agent': generate_user_agent()
+                    if not self.user_agent else self.user_agent}
 
-            if source is None:
-                self.in_q.task_done()
-                print('stopped get')
-                break
+        try:
+            func = getattr(self.session, source.method)
+            page = func(source.url, data=source.data,
+                        params=source.params,
+                        headers={**headers, # noqa
+                                    **source.headers})
+            # print(id(self), '{}'.format(source.url), page, source.method, source.data)
 
-            headers = {'User-Agent': generate_user_agent()
-                       if not self.user_agent else self.user_agent}
+            if page and source.parse:
+                source.data = page.text
+                return source
+            else:
+                print(source.url)
+                print(page)
+                print('No parsing required')
 
-            try:
-                func = getattr(self.session, source.method)
-                page = func(source.url, data=source.data,
-                            params=source.params,
-                            headers={**headers, # noqa
-                                     **source.headers})
-                self.visited += 1
-                self.total_time += page.elapsed.total_seconds()
-                self.mean = self.total_time / self.visited
-                # print(id(self), '{}'.format(source.url), page, source.method, source.data)
+            time.sleep(self.time_out)
+        # Retry later with a timeout,
+        except requests.Timeout:
+            print('timeout')
+            self.in_q.put(source)
 
-                if page and source.parse:
-                    source.data = page.text
-                    self.out_q.put(source)
-                else:
-                    print(source.url)
-                    print(page)
-                    print('No parsing required')
+        # Retry later with connection error.
+        except requests.ConnectionError:
+            print('connection error')
+            self.in_q.put(source)
 
-                time.sleep(self.time_out)
-            # Retry later with a timeout,
-            except requests.Timeout:
-                print('timeout')
-                self.in_q.put(source)
+            time.sleep(self.time_out)
 
-            # Retry later with connection error.
-            except requests.ConnectionError:
-                print('connection error')
-                self.in_q.put(source)
-
-                time.sleep(self.time_out)
-
-            except Exception as E:
-                print(E)
-                self.to_parse -= 1
-            self.in_q.task_done()
-
+        except Exception as E:
+            print(E)
+            self.to_parse -= 1
 
 #TODO fix the FileWorker class to the new spec.
 class FileWorker(Thread):
@@ -100,35 +101,21 @@ class FileWorker(Thread):
         self.in_q = in_q
         self.out_q = out_q
 
-    def run(self):
-        while True:
-            getter = self.in_q.get()
-            if template is None:
-                print('stopping')
-                break
-            with open(template.url) as fle:
-                lines = fle.readlines()
-                self.out_q.put((self.parser.parse(lines), db, col))
+    def retrieve(self, source):
+        with open(template.url) as fle:
+            lines = fle.readlines()
+        return lines
 
 
 class ProgramSource(BaseSourceWorker):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, function='', *args, **kwargs):
+        print(kwargs)
         super().__init__(*args, **kwargs)
-        self.mean = 0
-        self.total_time = 0
-        self.visited = 0
+        self.function = function
+        self.inits = {'function': function}
 
-    def run(self):
-        while True:
-            source = self.in_q.get()
-
-            if source is None:
-                self.in_q.task_done()
-                print('stopped', self.__name__)
-                break
-
-            result = subprocess.run(source.url, shell=True,
-                                    stdout=subprocess.PIPE)
-            source.data = result.stdout.decode('utf-8')
-            print(source.data)
-            self.out_q.put(source)
+    def retrieve(self, source):
+        result = subprocess.run(self.function + source.url, shell=True,
+                                stdout=subprocess.PIPE)
+        source.data = result.stdout.decode('utf-8')
+        return source
