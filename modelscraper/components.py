449bb0f6c016b62c64ffc30b5e1a26cb774b7945
Jim <jim@Anonymous.lan>
2018-01-24 12:38:05 +0100

    The Attr kws are now incrementally updated when a new instance is created. Templates has a new repr function

--- modelscraper/components.py
+++ modelscraper/components.py
@@ -1,3 +1,5 @@
+from itertools import zip_longest
+
 import requests
 import attr
 from attr.validators import instance_of
@@ -5,7 +7,7 @@ from attr.validators import instance_of
 from .sources import WebSource
 from .parsers import HTMLParser
 from .workers.store_worker import StoreWorker
-from .helpers import selector_converter, attr_dict, str_as_tuple
+from .helpers import selector_converter, attr_dict, str_as_tuple, wrap_list
 from . import databases
 
 
@@ -54,7 +56,7 @@ class Source(BaseModel):
 
 def source_conv(source):
     if source:
-        if source.__class__ == Source:
+        if type(source) == Source:
             return source
         return Source(**source) if type(source) == dict else Source()
 
@@ -69,7 +71,7 @@ class Attr(BaseModel):
     selector = attr.ib(default=None, convert=str_as_tuple)
     name = attr.ib(default=None)
     value = attr.ib(default=None, convert=str_as_tuple)
-    func = attr.ib(default=tuple, convert=str_as_tuple,
+    func = attr.ib(default=None, convert=str_as_tuple,
                    metadata={'Phase.parser': 1})
     attr_condition = attr.ib(default={})
     source_condition = attr.ib(default={})
@@ -82,13 +84,19 @@ class Attr(BaseModel):
     def __attrs_post_init__(self):
         # Ensure that the kws are encapsulated in a list with the same length
         # as the funcs.
-        if type(self.kws) not in [list, tuple]:
-            self.kws = (self.kws,)
+        if self.func:
+            self.kws = wrap_list(self.kws)
+            difference = len(self.func) - len(self.kws)
+            if difference:
+                self.kws = [*self.kws, *[{} for _ in range(difference)]]
 
-        difference = len(self.func) - len(self.kws)
-        if difference:
-            for _ in range(difference):
-                self.kws.append({})
+    def __call__(self, **kwargs):
+        new_kws = kwargs.get('kws')
+        if new_kws:
+            new_kws = wrap_list(new_kws)
+            kwargs['kws'] = [{**old, **new} for new, old in
+                             zip_longest(new_kws, self.kws, fillvalue={})]
+        return self.__class__(**{**self.__dict__, **kwargs})  # noqa
 
 
 @attr.s(repr=False)
@@ -111,12 +119,9 @@ class Template(BaseModel):
     preview = attr.ib(default=False)
 
     def __attrs_post_init__(self):
-        if self.db_type and not (self.db and self.table):
+        if self.db and not self.table:
             raise Exception(self.name +
-                'Database type is set, but not the names and the table')
-        elif (self.db or self.table) and not self.db_type:
-            raise Exception(self.name +
-                'Database name and table are set, but not the database type')
+                'Database name is set, but not the table')
 
         if self.url:
             self.attrs['url'] = Attr(name='url', value=self.url)
@@ -125,7 +130,7 @@ class Template(BaseModel):
         return {'url': self.url, **self.attrs_to_dict()} # noqa
 
     def attrs_to_dict(self):
-        return {attr.name: attr.value for attr in self.attrs.values()}
+        return {attr.name: attr.value for attr in self.attrs}
 
     def to_store(self):
         replica = self.__class__(db=self.db, table=self.table, func=self.func,
@@ -137,6 +142,7 @@ class Template(BaseModel):
     def attrs_from_dict(self, attr_dict):
         self.attrs = {name: Attr(name=name, value=value) for
                       name, value in attr_dict.items()}
+        print(self.attrs)
 
     def add_attr(self, attr):
         print('add_attr', attr.name)
@@ -144,10 +150,11 @@ class Template(BaseModel):
         self.attrs[attr.name] = attr
 
     def __repr__(self):
+        repr_string = self.name
         if self.objects:
             for objct in self.objects:
                 repr_string = "Template {}:\n".format(objct.name)
-                for attr in objct.attrs.values():
+                for attr in objct.attrs:
                     repr_string += "\t{}: {}\n".format(attr.name, attr.value)
         return repr_string
 
@@ -174,7 +181,7 @@ class ScrapeModel:
 
     def get_template(self, key):
         for phase in self.phases:
-            for template in run.templates:
+            for template in phase.templates:
                 if template.name == key:
                     return template
 
@@ -182,3 +189,11 @@ class ScrapeModel:
         template = self.get_template(template_name)
         database = databases._threads[template.db_type]()
         return database.read(template=template)
+
+    def __repr__(self):
+        repr_string = self.name + ':\n'
+        for phase in self.phases:
+            repr_string += '\t{}\n'.format(phase.name)
+            for template in phase.templates:
+                repr_string += '\t\t{}\n'.format(template.name)
+        return repr_string
