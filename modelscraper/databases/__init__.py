32c497758fa5b777fa151dd0066a26ebf62a0818
Jim <jim.lemmers@gmail.com>
2018-02-23 22:17:50 +0100

    the databases are now together in the databases file

--- modelscraper/databases/__init__.py
+++
@@ -1,8 +0,0 @@
-from .mongo_db import MongoDB
-from .shell_command import ShellCommand
-from .dummy import DummyDatabase
-
-_threads = {'mongo_db': MongoDB,
-            'shell_command': ShellCommand,
-            'dummy': DummyDatabase,
-            }
