8b9f19af5f1fa60928296a0e201598f61d0dead2
Jim <jim.lemmers@gmail.com>
2018-04-11 19:22:41 +0200

    made the selectors xpath instead of json stuff

--- scrape_models/ah_nl.py
+++ scrape_models/ah_nl.py
@@ -1,47 +1,47 @@
 from modelscraper.components import ScrapeModel, Phase, Template, Attr, Source
 from modelscraper.parsers import JSONParser
 from .objects.products import product_name, price, nutrition, brand_name,\
-    unitsize, availability, store_id, category
+    unitsize, availability, store_id, category, ingredients
 
-import string
-
+from string import ascii_lowercase
+import lxml.html
 
 base_search = 'https://www.ah.nl/service/rest/delegate?url=/zoeken?rq={}&searchType=product'
 base_url = 'https://www.ah.nl/{}'
 delegate_url = 'https://www.ah.nl/service/rest/delegate?url={}'
-search = (Source(url=base_search.format(l)) for l in string.ascii_lowercase)
+table_trans = str.maketrans('[]', '<>')
+search = (Source(url=base_search.format(l)) for l in ascii_lowercase)
 
-product_selector = ['type:ProductDetailLane', '_embedded', 'items',
-                    0, '_embedded','product']
-
+product_selector = '//type[text() = "ProductDetailLane"]/..//type[text() = "Product"]/..'
+paragraph_selector = '//content//title[text() = "{}"]/../../../item[last()]/text/body'
 product = Template(
     name='product',
     db='ah_nl',
     table='products',
-    selector=['_embedded', 'lanes', '*'],
     attrs=[
-        nutrition(func='sel_text',
-            selector=[8, '_embedded', 'items', 1,'_embedded','sections',0,
-                      '_embedded', 'content', 2, 'text', 'body']),
-
-        product_name(selector=product_selector + ['description']),
-        brand_name(selector=product_selector + ['brandName']),
-        unitsize(selector=product_selector + ['unitSize']),
-        availability(func='sel_value',
-                     selector=product_selector + ['availability', 'orderable']),
-        price(func='sel_value',
-              selector=product_selector + ['priceLabel', 'now']),
-        store_id(selector=product_selector + ['id']),
-        category(selector=product_selector + ['categoryName']),
+        nutrition(
+            func=['sel_text', 'custom_func', 'sel_table'],
+            kws=[{}, {'function': lambda text: text.translate(table_trans),
+                      'selector': 'td'}],
+            selector=paragraph_selector.format('Voedingswaarden'),
+        ),
+        ingredients(selector=paragraph_selector.format("Ingrediënten")),
+        product_name(selector=product_selector + '//description'),
+        brand_name(selector=product_selector + '//brandName'),
+        unitsize(selector=product_selector + '//unitSize'),
+        availability(selector=product_selector + '//availability/orderable'),
+        price(selector=product_selector + '//priceLabel/now'),
+        store_id(selector=product_selector + '//id'),
+        category(selector=product_selector + '//categoryName'),
         ]
 )
 
 product_url = Attr(
     name='url',
-    selector=['navItem', 'link', 'href'],
+    selector='navItem/link/href',
     func='sel_text',
     kws={'template': delegate_url},
-    source={'active': False}
+    source={'active': False},
 )
 
 search_template = Template(
@@ -49,20 +49,20 @@ search_template = Template(
     db_type='MongoDB',
     db='ah_nl',
     table='product_urls',
-    selector=['_embedded', 'lanes', 'type:SearchLane', '_embedded', 'items'],
+    selector='//type[text() = "SearchLane"]/../_embedded/items/item',
     attrs=[product_url]
 )
 
 load_more = Attr(
     name='load_more_url',
-    selector=['navItem', 'link', 'href'],
+    selector='navItem/link/href',
     func='sel_text',
     source=Source(src_template='https://www.ah.nl/{}')
 )
 
 load_more_template = Template(
     name='load_more',
-    selector=['_embedded', 'lanes', 7],
+    selector='//type[text() = "LoadMoreLane"]/..',
     attrs=[load_more]
 )
 
