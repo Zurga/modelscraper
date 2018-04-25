dea701e753fa13569ae47cab286bc051f8cc1bd7
Jim <jim.lemmers@gmail.com>
2018-04-25 16:54:56 +0200

    removed the dispatcher

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ import pprint
 import click
 
 from modelscraper.components import ScrapeModel
-from modelscraper.dispatcher import Dispatcher
 
 
 if 'banner.txt' in os.listdir():
@@ -22,17 +21,16 @@ available_models = sorted([f[:-3] for f in os.listdir('scrape_models')
 def main(model, dummy):
     if len(model) == 1:
         model = model[0]
-    dispatcher = Dispatcher()
     if model not in available_models:
         print('Model', model, 'is not in the folder "scrape_models".')
         print('These models are available:')
         pprint.pprint(available_models, compact=True)
         return
     imported = vars(importlib.import_module('scrape_models.%s' % model)).values()
-    scrape_models = [model for model in imported
-                     if type(model) == ScrapeModel]
-    dispatcher.add_scraper(scrape_models, dummy=dummy)
-    dispatcher.run()
+    scrape_models = (model for model in imported
+                     if type(model) == ScrapeModel)
+    for model in scrape_models:
+        model.run()
 
 if __name__ == '__main__':
     main()
