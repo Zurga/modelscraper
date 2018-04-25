333fb4f90184e57175b5f06765c104f4d046df5b
Jim <jim.lemmers@gmail.com>
2018-04-25 12:50:43 +0200

    updated the inits

--- modelscraper/sources.py
+++ modelscraper/sources.py
@@ -7,23 +7,22 @@ from user_agent import generate_user_agent
 
 
 class BaseSourceWorker(Thread):
-    def __init__(self, in_q, out_q, lock, to_parse, id=0):
+    def __init__(self, id=None, in_q=None, out_q=None, lock=None, to_parse=None):
         super().__init__()
-        self.id  = id
+        self.id = id
         self.in_q = in_q
         self.out_q = out_q
         self.lock = lock
-        self.mean = 0
+        self.to_parse = to_parse
+        self.mean =0
         self.total_time = 0
         self.visited = 0
         self.retrieving = False
-        self.to_parse = to_parse
 
     def __call__(self, **kwargs):
         return self.__class__(**kwargs, **self.inits)  # noqa
 
     def run(self):
-        print('started', self.__class__.__name__, id(self))
         while True:
             start = time.time()
             source = self.in_q.get()
@@ -31,7 +30,7 @@ class BaseSourceWorker(Thread):
                 break
             self.retrieving = True
             source, increment = self.retrieve(source)
-            self.visited += 1
+            self._recalculate_mean(start)
 
             if source:
                 self.out_q.put(source)
@@ -47,6 +46,7 @@ class BaseSourceWorker(Thread):
         raise NotImplementedError
 
     def _recalculate_mean(self, start):
+        self.visited += 1
         self.total_time += time.time() - start
         return self.total_time / self.visited
 
@@ -57,23 +57,23 @@ class WebSource(BaseSourceWorker):
     It will place all items in its queue into the out_q specified.
     For use without parent Thread supply keyword arguments: name, domain, in_q,
     '''
-    def __init__(self, time_out=0.3, retries=10, session=requests.Session(),
-                 user_agent='', **kwargs):
-        super().__init__(**kwargs)
-        self.name = 'WebSource ' + str(self.id)
+    def __init__(self, retries=10, session=requests.Session(), time_out=0.3,
+                 user_agent='', *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.retries = retries
         self.session = session
         self.time_out = time_out
         self.user_agent = user_agent
-        self.inits = {'session': session,
-                      'time_out': time_out,
-                      'retries': retries,
-                      'user_agent': user_agent}
+        self.inits = {
+            'session': self.session,
+            'time_out': self.time_out,
+            'retries': self.retries,
+            'user_agent': self.user_agent,
+        }
 
     def retrieve(self, source):
         headers = {'User-Agent': generate_user_agent()
                     if not self.user_agent else self.user_agent}
-
         try:
             time.sleep(self.time_out)
             func = getattr(self.session, source.method)
@@ -105,9 +105,6 @@ class WebSource(BaseSourceWorker):
 
 
 class FileSource(BaseSourceWorker):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def retrieve(self, source):
         try:
             with open(source.url) as fle:
@@ -118,15 +115,22 @@ class FileSource(BaseSourceWorker):
 
 
 class ProgramSource(BaseSourceWorker):
-    def __init__(self, function='', **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, function=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.function = function
-        self.inits = {'function': function}
+        self.inits = {'function': self.function}
 
     def retrieve(self, source):
-        result = subprocess.run(self.function + source.url, shell=True,
+        function = self.function.format(source.url)
+        result = subprocess.run(function, shell=True,
                                 stdout=subprocess.PIPE)
-        source.data = result.stdout.decode('utf-8')
+        try:
+            stdout = result.stdout
+            source.data = stdout.decode('utf-8')
+        except Exception as E:
+            logging.log(logging.WARNING, 'Could not decode the result from ' +
+                        function + ':\n ' + stdout)
+            return False, -1
         return source, 1
 
 
