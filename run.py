5b072457a788b5b753ae8004cc4e9fabb68f4ece
Jim <jim.lemmers@gmail.com>
2018-04-30 01:36:06 +0200

    adapted to the new dummy behaviour

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def main(model, dummy):
     scrape_models = (model for model in imported
                      if type(model) == ScrapeModel)
     for model in scrape_models:
-        model.run()
+        model.run(dummy=dummy)
 
 if __name__ == '__main__':
     main()
