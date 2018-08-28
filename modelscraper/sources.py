6dcaef68dbe01c52aac41af54dca36b90aab91d9
Jim <jim.lemmers@gmail.com>
2018-08-28 17:21:48 +0200

    moved the source_workers to a different file and added specific sources for their respective types

--- modelscraper/sources.py
+++ modelscraper/sources.py
@@ -1,192 +1,134 @@
-from threading import Thread
-import time
-import subprocess
+import attr
 import importlib
 import logging
+import subprocess
+import time
+from queue import Queue, Empty
+from threading import Thread
 
-import requests
 from user_agent import generate_user_agent
+import requests
+from pybloom_live import ScalableBloomFilter
+
+from .source_workers import WebSourceWorker, FileSourceWorker, \
+    ProgramSourceWorker, ModuleSourceWorker, APISourceWorker
 
 
-class BaseSourceWorker(Thread):
-    def __init__(self, id=None, in_q=None, out_q=None, lock=None, to_parse=None):
-        super().__init__()
-        self.id = id
-        self.in_q = in_q
-        self.out_q = out_q
-        self.lock = lock
-        self.to_parse = to_parse
-        self.mean =0
-        self.total_time = 0
-        self.visited = 0
-        self.retrieving = False
+@attr.s(hash='in_q')
+class Source(object):
+    name = attr.ib('')
+    compression = attr.ib('')
+    duplicate = attr.ib(False)
+    func = attr.ib('get')
+    kws = attr.ib(attr.Factory(dict))
+    n_workers = attr.ib(default=1)
+    in_q = attr.ib(attr.Factory(Queue))
+    out_q = attr.ib(attr.Factory(Queue))
+    url_template = attr.ib('{}')
+    urls = attr.ib(attr.Factory(list))
+    seen = attr.ib(attr.Factory(ScalableBloomFilter))
+    to_parse = attr.ib(0)
+    lock = attr.ib(None)
+    attrs = attr.ib(attr.Factory(list))
 
-    def __call__(self, **kwargs):
-        return self.__class__(**kwargs, **self.inits)  # noqa
+    def __attrs_post_init__(self):
+        # Initialize the worker
+        self.workers = [
+            self.source_worker(parent=self, id=1, in_q=self.in_q,
+                               out_q=self.out_q, lock=self.lock,
+                               to_parse=self.to_parse)
+            for _ in range(self.n_workers)]
+        for worker in self.workers:
+            worker.start()
+        self.url_amount = int((self.n_workers / 2) + 2)
+        if not self.urls:
+            self.received = False
+        else:
+            self.received = True
 
-    def run(self):
-        while True:
-            start = time.time()
-            source = self.in_q.get()
-            if source is None:
-                break
-            self.retrieving = True
-            source, increment = self.retrieve(source)
-            self._recalculate_mean(start)
+    @classmethod
+    def from_db(cls, template, url='url', query={}, **kwargs):
+        db_type = template.db_type[0]
 
-            if source:
-                self.out_q.put(source)
-
-            with self.lock:
-                self.to_parse += increment
-
-            self.in_q.task_done()
-            self.retrieving = False
-
-    def retrieve(self):
-        raise NotImplementedError
-
-    def _recalculate_mean(self, start):
-        self.visited += 1
-        self.total_time += time.time() - start
-        return self.total_time / self.visited
-
-
-class WebSource(BaseSourceWorker):
-    '''
-    This class is used as a downloader for pages.
-    It will place all items in its queue into the out_q specified.
-    For use without parent Thread supply keyword arguments: name, domain, in_q,
-    '''
-    def __init__(self, retries=10, session=requests.Session(), time_out=0.3,
-                 user_agent='', *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.retries = retries
-        self.session = session
-        self.time_out = time_out
-        self.user_agent = user_agent
-        self.inits = {
-            'session': self.session,
-            'time_out': self.time_out,
-            'retries': self.retries,
-            'user_agent': self.user_agent,
-        }
-
-    def retrieve(self, source):
-        headers = {'User-Agent': generate_user_agent()
-                    if not self.user_agent else self.user_agent,
-                   **source.kws.pop('headers', {})}
-        try:
-            time.sleep(self.time_out)
-            func = getattr(self.session, source.func)
-            page = func(source.url, headers=headers, **source.kws) # noqa
-
-            if page and source.parse:
-                source.data = page.text
-                return source, 1
+        # Check if the database has been instantiated by the Scrapeworker
+        if isinstance(db_type, type):
+            db_type = db_type()
+        elif type(db_type) is str:
+            db_type = getattr(databases, db_type)()
+        for t in db_type.read(template=template, query=query):
+            attr = t.attrs.get(url, [])
+            if type(attr.value) is not list:
+                values = [attr.value]
             else:
-                return False, -1
+                values = attr.value
+            for v in values:
+                yield cls(url=v, **kwargs)
 
-        # Retry later with a timeout,
-        except requests.Timeout:
-            self.in_q.put(source)
-            return False, 0
-
-        # Retry later with connection error.
-        except requests.ConnectionError:
-            self.in_q.put(source)
-            time.sleep(self.time_out)
-            return False, 0
-
-        except Exception as E:
-            print(self.__class__.__name__, id(self), E)
-            return False, -1
-
-
-class FileSource(BaseSourceWorker):
-    def retrieve(self, source):
+    def __iter__(self):
+        print('consume')
+        self.consume()
         try:
-            with open(source.url) as fle:
-                source.data = fle.read()
-        except FileNotFoundError:
-            return False, -1
-        return source, 1
+            url, attrs, data = self.out_q.get(timeout=2)
+            print('__iter__', url)
+            self.out_q.task_done()
+            yield url, attrs, data
+        except Empty:
+            print(self.name, 'is empty')
+            if self.received and not all(w.retrieving for w in self.workers):
+                print('stopping source', self.name)
+                yield None
 
+    def consume(self):
+        if self.urls:
+            if type(self.urls) is list:
+                for _ in range(self.url_amount):
+                    try:
+                        url = self.urls.pop()
+                    except IndexError:
+                        self.urls = False
+            else:
+                for _ in range(self.url_amount):
+                    try:
+                        url = next(self.urls)
+                    except StopIteration:
+                        self.urls = False
 
-class ProgramSource(BaseSourceWorker):
-    def retrieve(self, source):
-        function = source.func.format(source.url)
-        result = subprocess.run(function, shell=True,
-                                stdout=subprocess.PIPE)
-        try:
-            stdout = result.stdout
-            source.data = stdout.decode('utf-8')
-            return source, 1
-        except Exception as E:
-            logging.log(logging.WARNING, 'Could not decode the result from ' +
-                        function + ':\n ' + stdout)
-            return False, -1
+            if self.attrs and type(self.attrs) is list:
+                attrs = self.attrs.pop()
+            else:
+                attrs = {}
 
-class ModuleSource(BaseSourceWorker):
+            url = self.url_template.format(url)
+            self.in_q.put((url, attrs))
+            self.seen.add(url)
 
-    """Generates data by calling another modules function."""
+    def add_source(self, url, attrs):
+        self.received = True
+        url = self.url_template.format(url)
+        if url not in self.seen or self.repeat:
+            print('adding', url)
+            self.in_q.put((url, attrs))
+            self.seen.add(url)
+            print(self.in_q.qsize())
 
-    def __init__(self, module=None, conversion=None, *args, **kwargs):
-        """@todo: to be defined1.
+@attr.s
+class WebSource(Source):
+    source_worker = WebSourceWorker
+    retries = attr.ib(10)
+    session = attr.ib(requests.Session())
+    domain = attr.ib('')
+    user_agent = attr.ib('')
+    cookies = attr.ib(attr.Factory(dict))
+    time_out = attr.ib(1)
 
-        :module_name: @todo
+class FileSource(Source):
+    source_worker = FileSourceWorker
 
-        """
-        super().__init__(*args, **kwargs)
+class ProgramSource(Source):
+    source_worker = ProgramSourceWorker
 
-        self.module = module
-        self.conversion = conversion
-        self.inits = {'module': self.module,
-                      'conversion': conversion}
+class ModuleSource(Source):
+    source_worker = ModuleSourceWorker
 
-    def retrieve(self, source):
-        """Returns the data gotten by the source
-
-        :source: @todo
-        :returns: @todo
-
-        """
-        for name in source.func.split('.'):
-            function = getattr(self.module, name)
-        try:
-            source.data = function(source.url, **source.kws)
-            if self.conversion:
-                source.data = self.conversion(source.data)
-            return source, 1
-        except Exception as E:
-            logging.warning(' : '.join([str(E), source.url, str(source.kws)]))
-            return False, -1
-
-
-class APISource(BaseSourceWorker):
-    def __init__(self, api_function=None, batch=1, **kwargs):
-        super().__init__(**kwargs)
-        self.api_function = api_function
-        self.batch = batch
-
-    def retrieve_batch(self, sources):
-        return self.api_function(source[0] if type(sources) == list else source)
-
-    def run(self):
-        print('started', self.__class__.__name__, id(self))
-        while True:
-            start = time.time()
-            source = self.in_q.get()
-            if source is None:
-                break
-            self.retrieving = True
-            source = self.retrieve(source)
-            # source = self.retrieve(self.in_q.get())
-            if source:
-                self.out_q.put(source)
-            self.visited += 1
-            self.total_time += time.time() - start
-            self.mean = self.total_time / self.visited
-            self.in_q.task_done()
-            self.retrieving = False
-
+class APISource(Source):
+    source_worker = APISourceWorker
