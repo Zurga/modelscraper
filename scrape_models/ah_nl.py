7c0bc0a5ea5a56f1418452e27da2b5271c7aeb04
Jim <jim.lemmers@gmail.com>
2018-09-14 23:12:20 +0200

    fixed to new model spec

--- scrape_models/ah_nl.py
+++ scrape_models/ah_nl.py
@@ -1,31 +1,71 @@
-from modelscraper.components import ScrapeModel, Phase, Template, Attr, Source
+from modelscraper.components import ScrapeModel, Template, Attr
+from modelscraper.sources import WebSource, FileSource
 from modelscraper.parsers import JSONParser
-from modelscraper.sources import FileSource
+from modelscraper.databases import MongoDB
 from .objects.products import product_name, price, nutrition, brand_name,\
     unitsize, availability, store_id, category, ingredients
 
 from string import ascii_lowercase
-import lxml.html
 
-base_search = 'https://www.ah.nl/service/rest/delegate?url=/zoeken?rq={}&searchType=product'
-base_url = 'https://www.ah.nl/{}'
+base_search = 'service/rest/delegate?url=/zoeken?rq={}&searchType=product'
 delegate_url = 'https://www.ah.nl/service/rest/delegate?url={}'
 table_trans = str.maketrans('[]', '<>')
-search = (Source(url=base_search.format(l)) for l in ascii_lowercase)
-test_product = (Source('data/ah_nl/products.json'),)
+
+search = WebSource(name='search',
+                   url_template='https://www.ah.nl/{}',
+                   urls=(base_search.format(l) for l in ascii_lowercase))
+#more_results = WebSource(url_template='https://www.ah.nl/{}')
+
+product_test = 'producten/product/wi238928/ah-biologisch-schouderkarbonade'
+product_source = WebSource(name='product_source', url_template=delegate_url,
+                           test_urls=[product_test])
+test_product = FileSource(urls=['data/ah_nl/products.json'])
+db = MongoDB(db='ah_nl')
+
+search_template = Template(
+    source=search,
+    name='search_result',
+    database=db,
+    table='product_urls',
+    selector='//type[text() = "SearchLane"]/../_embedded/items',
+    parser=JSONParser,
+    attrs=[
+        Attr(
+            name='url',
+            selector='navItem/link/href',
+            func='sel_text',
+            emits=product_source
+        )
+    ]
+)
+
+load_more_template = Template(
+    source=search,
+    name='load_more',
+    selector='//type[text() = "LoadMoreLane"]/..',
+    parser=JSONParser,
+    attrs=[
+        Attr(
+            name='url',
+            selector='navItem/link/href',
+            func='sel_text',
+            emits=search)
+    ]
+)
 
 product_selector = '//type[text() = "ProductDetailLane"]/..//type[text() = "Product"]/..'
 paragraph_selector = '//content//title[text() = "{}"]/../../../content[last()]/text/body'
 product = Template(
+    source=product_source,
     name='product',
-    db='ah_nl',
     table='products',
-    db_type='MongoDB',
+    database=db,
+    parser=JSONParser,
     attrs=[
         nutrition(
             func=['sel_text', 'custom_func', 'sel_table'],
-            kws=[{}, {'function': lambda text: text.translate(table_trans),
-                      'selector': 'td'}, {}],
+            kws=[{}, {'function': lambda text: text.translate(table_trans)},
+                 {}],
             selector=paragraph_selector.format('Voedingswaarden'),
         ),
         ingredients(selector=paragraph_selector.format("Ingrediënten")),
@@ -36,46 +76,11 @@ product = Template(
         price(selector=product_selector + '//priceLabel/now'),
         store_id(selector=product_selector + '//id'),
         category(selector=product_selector + '//categoryName'),
+        Attr(name='url', from_source=True)
         ]
 )
 
-product_url = Attr(
-    name='url',
-    selector='navItem/link/href',
-    func='sel_text',
-    kws={'template': delegate_url},
-    source={'active': False},
-)
-
-search_template = Template(
-    name='search_result',
-    db_type='MongoDB',
-    db='ah_nl',
-    table='product_urls',
-    selector='//type[text() = "SearchLane"]/../_embedded/items',
-    attrs=[product_url]
-)
-
-load_more = Attr(
-    name='load_more_url',
-    selector='navItem/link/href',
-    func='sel_text',
-    source=Source(src_template='https://www.ah.nl/{}')
-)
-
-load_more_template = Template(
-    name='load_more',
-    selector='//type[text() = "LoadMoreLane"]/..',
-    attrs=[load_more]
-)
-
-
 ah = ScrapeModel(
     name='ah', domain='https://www.ah.nl/',
-    phases=[
-        Phase(name='Search', parser=JSONParser, sources=search,
-             templates=[search_template, load_more_template]),
-        Phase(name='products',
-              parser=JSONParser, templates=[product])
-    ]
+    templates=[search_template, load_more_template, product]
 )
