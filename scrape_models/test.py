485b08f5c1138177676d632d82310636623e6185
Jim <jim.lemmers@gmail.com>
2018-10-13 19:23:00 +0200

    updated to new spec

--- scrape_models/test.py
+++ scrape_models/test.py
@@ -1,56 +1,65 @@
-from modelscraper.components import ScrapeModel, Template, Attr
-from modelscraper.sources import FileSource
+from modelscraper.components import Scraper, Template, Attr
+from modelscraper.sources import FileSource, BrowserSource, WebSource
 from modelscraper.parsers import HTMLParser, JSONParser, TextParser, CSVParser
 from modelscraper.databases import MongoDB, Sqlite
-import http.server
 
 
+#Sources
 json_test = FileSource(urls=['test/json'])
-html = FileSource(name='html', urls=['test/html'], attrs=[{'test': ['value']}])
+html = BrowserSource(name='html', urls=['http://google.com'], attrs=[{'test': ['value']}])
+#html = FileSource(name='html', urls=['test/html' ], attrs=[{'test': ['value']}])
 html2 = FileSource(name='html2')
 json_html_nest = FileSource(urls=['test/json_html_nested'])
 text_source = FileSource(name='text_source', urls=['test/text'])
 csv_source = FileSource(name='csv_source', urls=['test/csv'])
+
+
+#Databases
 test_db = Sqlite(db='test')
 test_mongo = MongoDB(db='test')
+
+#Parsers
+jsonp = JSONParser()
+textp = TextParser()
+csvp = CSVParser()
+htmlp = HTMLParser()
 
 json_nested = Template(
     source=json_test,
     database=[test_db, test_mongo], table='tst',
-    name='json_nested', parser=[JSONParser, HTMLParser],
-    selector=['html', '.content'], attrs=[
-        Attr(name='url', selector='h1', func=['sel_text', 'sel_text'],
-             kws=[{}, {'template': 'partialtest {}'}]),
+    name='json_nested',
+    selector=[jsonp.select('html'), htmlp.select('.content')], attrs=[
+        Attr(name='url',
+             func=htmlp.text(selector='h1', template='partialtest {}'))
     ])
 
 html_functions = Template(
     source=html,
-    name='html_functions', parser=HTMLParser,
-    database=test_db, table='html_test',
-    dated=True,
-    selector='html', attrs=[
-        Attr(name='table' , selector='table', func='sel_table'),
-        Attr(name='attr' , selector='p', func='sel_attr',
-             kws={'attr': 'class'}),
-        Attr(name='url' , selector='a', func='sel_url', emits=html2),
-        Attr(name='exists', selector='p', func='sel_exists', kws={'key': 'Help'}),
-        Attr(name='js_array', selector='script', func='sel_js_array'),
+    name='html_functions',
+    database=test_mongo, table='html_test',
+    dated=True, emits=html2,
+    selector=htmlp.select('html'), attrs=[
+        Attr(name='table', func=htmlp.table(selector='table')),
+        Attr(name='attr', func=htmlp.attr(selector='p', attr='class')),
+        Attr(name='url', func=htmlp.url(selector='a')),
+        Attr(name='exists', func=htmlp.exists(selector='p', key='Help')),
+        Attr(name='js_array', func=htmlp.js_array(selector='script')),
     ])
 
 html2_func = Template(
     source=html2,
     name='html_functions2', parser=HTMLParser,
-    database=test_db, table='html_test',
+    database=test_mongo, table='html_test',
     dated=True,
-    selector='html', attrs=[
-        Attr(name='table' , selector='table', func='sel_table'),
-        Attr(name='attr' , selector='p', func='sel_attr',
-             kws={'attr': 'class'}),
-        Attr(name='url' , selector='a', func='sel_url' ),
-        Attr(name='exists', selector='p', func='sel_exists', kws={'key': 'Help'}),
-        Attr(name='js_array', selector='script', func='sel_js_array'),
+    attrs=[
+        Attr(name='table', func=htmlp.table(selector='table')),
+        Attr(name='attr', func=htmlp.attr(selector='p', attr='class')),
+        Attr(name='url', func=htmlp.url(selector='a')),
+        Attr(name='exists', func=htmlp.exists(selector='p', key='Help')),
+        Attr(name='js_array', func=htmlp.js_array(selector='script')),
     ])
 
+'''
 text_functions = Template(
     source=text_source,
     name='text_functions', parser=TextParser,
@@ -69,9 +78,10 @@ csv_functions = Template(
         Attr(name='col2', selector=2, func='sel_text'),
         Attr(name='col1' , selector=1, func='sel_text'),
     ])
+'''
 
 
-test = ScrapeModel(
+test = Scraper(
     name='test', domain='http://localhost:9999',
     templates=[html_functions, html2_func]
     , logfile='test.log')
