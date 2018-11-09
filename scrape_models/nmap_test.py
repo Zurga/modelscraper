25958520037902f2ed360c39f23f0877c4054419
Jim <jim.lemmers@gmail.com>
2018-11-09 11:58:41 +0100

    updated to new spec

--- scrape_models/nmap_test.py
+++ scrape_models/nmap_test.py
@@ -1,19 +1,21 @@
 from modelscraper.components import Scraper, Model, Attr
 from modelscraper.sources import ProgramSource
+from modelscraper.parsers import HTMLParser
 from modelscraper.databases import MongoDB
 
 
-nmap_source = ProgramSource(urls=['localhost'], func='nmap -oX - {}')
+nmap_source = ProgramSource(urls=['localhost'], test_urls=['localhost'], func='nmap -oX - {}')
+parser = HTMLParser()
+
 
 port_template = Model(
     source=nmap_source,
-    name='ports', selector='port',
-    database=MongoDB('nmap'),
+    name='ports', selector=parser.select('port'),
+    database=MongoDB(db='nmap'),
     table='ports', attrs=(
-    Attr(name='portnumber', func='sel_attr',
-         kws={'attr': 'portid'}),
-    Attr(name='state', selector='state', func='sel_attr',
-         kws={'attr': 'state'}),
-    Attr(name='service', selector='service', func='sel_attr',
-         kws={'attr': 'name'})))
+    Attr(name='portnumber', func=parser.attr(attr='portid')),
+    Attr(name='state', func=parser.attr(selector='state', attr='state')),
+    Attr(name='service', func=parser.attr(selector='service',attr='name')))
+)
+
 nmap = Scraper(name='nmap_test', models=[port_template])
