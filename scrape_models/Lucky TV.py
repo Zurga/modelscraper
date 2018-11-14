8a775137656168110e7f6f4ca8e9104a1b43e836
Jim <jim.lemmers@gmail.com>
2018-11-14 12:28:11 +0100

    updated to new spec

--- scrape_models/Lucky TV.py
+++ scrape_models/Lucky TV.py
@@ -1,32 +1,22 @@
-from modelscraper.dispatcher import Dispatcher
-from modelscraper.components import ScrapeModel, Phase, Template, Attr, Source
-from pymongo import MongoClient
-from modelscraper.sources import WebSource
-from modelscraper.parsers import HTMLParser
+from modelscraper import Scraper, Model, Attr, WebSource, HTMLParser, Sqlite
 
 
-cl = MongoClient()
-db = cl.lucky_tv
-col = db.episode
+sources = WebSource(urls=("http://www.luckytv.nl/afleveringen/page/{}/".format(i)
+           for i in range(1, 50)))
+db = Sqlite(db='luckytv')
+html = HTMLParser()
 
-sources = (Source(url="http://www.luckytv.nl/afleveringen/page/{}/".format(i))
-           for i in range(1, 50))
+episode = Model(
+    source=sources,
+    name='episode', selector=html.select('article.video'),
+    database=db, table='episodes',
+    attrs=(
+        Attr(name='url', func=html.url(selector='a:nth-of-type(1)')),
+        Attr(name='title', func=html.text(selector='.video__title')),
+        Attr(name='date', func=html.text(selector='.video__date')),
+    )
+)
 
-LuckyTV = ScrapeModel(name='Lucky TV', domain='http://www.luckytv.nl/',
-    num_getters=2, phases=[
-    Phase(source_worker=WebSource, parser=HTMLParser, sources=sources,
-        templates=(
-            Template(
-                name='episode', selector='article.video',
-                db_type='MongoDB', db='lucky_tv', table='episodes',
-                attrs=(
-                    Attr(name='url', selector='a:nth-of-type(1)',
-                         func='sel_url'),
-                    Attr(name='title', selector='.video__title',
-                         func='sel_text'),
-                    Attr(name='date', selector='.video__date', func='sel_text'),
-                )
-            ),
-        )
-    ),
-])
+LuckyTV = Scraper(
+    name='Lucky TV',
+    models=[episode])
