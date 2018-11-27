aa1ca5838e2bc96f8a90ebad8bf349291638debe
Jim <jim.lemmers@gmail.com>
2018-11-27 15:59:11 +0100

    added influx db import

--- modelscraper/__init__.py
+++ modelscraper/__init__.py
@@ -1,5 +1,5 @@
 from .sources import WebSource, BrowserSource, ModuleSource, ProgramSource, APISource
-from .databases import MongoDB, ShellCommand, CSV, Sqlite, File
+from .databases import MongoDB, ShellCommand, CSV, Sqlite, File, InfluxDB
 from .parsers import HTMLParser, JSONParser, CSVParser, TextParser
 from .selectors import TextSelector, JavascriptVarSelector, ORCSSSelector, SliceSelector
 from .components import Model, Attr, Scraper
