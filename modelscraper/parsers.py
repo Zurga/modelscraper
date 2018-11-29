1f89e0a8efd4cee1abc8faef3118b2563a18c30b
Jim <jim.lemmers@gmail.com>
2018-11-29 15:26:18 +0100

    added documentation, bugfixes

--- modelscraper/parsers.py
+++ modelscraper/parsers.py
@@ -1,24 +1,19 @@
 from datetime import datetime
-import csv
 import json
 import re
 import logging
 from functools import partial
-from logging.handlers import QueueHandler
-from queue import Queue
 from urllib import parse as urlparse
-from functools import singledispatch
 
 import lxml.html as lxhtml
 import lxml.etree as etree
-import lxml
 from lxml.cssselect import CSSSelector
 from cssselect import SelectorSyntaxError
 
 from scrapely import Scraper
-import xmljson
+from jq import jq, _Program
 
-from .helpers import str_as_tuple, add_other_doc, wrap_list
+from .helpers import add_other_doc, wrap_list, format_docstring
 from .selectors import ORCSSSelector, JavascriptVarSelector
 
 
@@ -39,20 +34,52 @@ class BaseParser(object):
             if selector:
                 selected = selector(data)
                 if not selected:
-                    self.logger.warning(str(selector) + 'selected nothing')
+                    self.logger.warning(str(selector) + 'selected nothing' + str(url))
                     if debug:
                         print(etree.tostring(data))
                 return selected
             return (data,)
         return []
 
-    def _modify_text(self, text, replacers=None, substitute='', regex: str='',
-                numbers: bool=False, needle=None, template=''):
-        text = text.strip()
-        if replacers:
-            for key, subsitute in zip(replacers, substitute):
-                text = text.replace(key, substitute)
+    def _modify_text(self, text: str, replacers=None, substitute='', regex:
+                     str='', numbers: bool=False, template: str='',
+                     translation_table: dict={}):
+        '''
+        replacers : list of str, optional
+                    Must be used with the substitute parameter.
+                    For each key found in the replacers list,
+                    the value found in the substitute list will be set
+                    with the help of the str.replace function.
 
+        substitute : list of str, optional
+                    Must be used with the replacers parameter.
+                    The value to be substituted for each replacement character
+                    found in the replacers parameter
+
+        regex : str, optional
+                The regular expression provided here will be executed against
+                the text. The regular expression is executed with a
+                re.findall().  All the text which matched will be concatenated
+                with ''.join().
+
+        numbers : bool, optional
+                  If set to True, only numbers found in the text will be
+                  returned.
+
+        template : str, optional
+                   A template string that will be applied with the str.format
+                   method to the text found.
+
+        translation : dict, optional
+                   This will execute the str.translation with translation_table
+                   provided on the text found. :seealso:python.str.maketrans.
+
+        Note
+        ----
+        Order of occurence for the methods used by setting each parameter:
+            regex -> replacers -> translation -> numbers -> template.
+        '''
+        text = text.strip()
         if regex:
             regex = re.compile(regex)
             try:
@@ -60,10 +87,12 @@ class BaseParser(object):
             except:
                 print('regex error', text)
 
-        if needle:
-            matches = re.match(re.escape(needle), text)
-            if not matches:
-                return None
+        if replacers and substitute:
+            for key, subsitute in zip(replacers, substitute):
+                text = text.replace(key, substitute)
+
+        if translation_table:
+            text = text.translate(translation_table)
 
         if numbers and any(map(str.isdecimal, text)):
             text = int(''.join([c for c in text if c.isdecimal() and c]))
@@ -90,8 +119,11 @@ class BaseParser(object):
         Selects and modifies text.
         '''
         for element in self._select(url, data, selector):
-            stripped = element.lstrip().rstrip()
-            yield self._modify_text(stripped, **kwargs)
+            if element:
+                stripped = str(element).lstrip().rstrip()
+                yield self._modify_text(stripped, **kwargs)
+            else:
+                yield ''
 
     def exists(self, selector=None, key: str='', **kwargs):
         '''
@@ -119,23 +151,47 @@ class BaseParser(object):
             yield function(element)
 
 
+_HTMLParser_selector_doc = '''
+            A string which represents a selector that can be used by
+            this parser. It will be applied to the data to select the correct
+            elements.  '''
+
+
 class HTMLParser(BaseParser):
     '''
     A parser that is able to parse both XML and HTML.
-    '''
 
-    data_types = (lxhtml.HtmlElement, etree._Element, lxhtml.FormElement)
+    Attributes
+    ----------
+    selectors : (CSSSelector, ORCSSSelector, etree.XPath, JavascriptVarSelector)
+                The type of selectors that can be used with this parsers
+                "selectors" keyword argument. If a string is provided, this
+                string will be converted into a CSSSelector or an XPath
+                selector.
+    '''
+    _data_types = (lxhtml.HtmlElement, etree._Element, lxhtml.FormElement)
     _table_row_selector = ORCSSSelector('th', 'td')
-    selectors = (CSSSelector, ORCSSSelector, etree.XPath, JavascriptVarSelector)
-    def __init__(self, parse_escaped=True):
+    selectors = (CSSSelector, ORCSSSelector, etree.XPath,
+                 JavascriptVarSelector)
+
+    def __init__(self, parse_escaped: bool=True):
+        '''
+        Parameters
+        ----------
+        parse_escaped : bool, optional
+                        If set to True, '&lt;' and '&gt;' will be converted to
+                        '<' and '>' respectively.
+        '''
         super().__init__()
         self.scrapely_parser = None
         self.parse_escaped = parse_escaped
 
     def _convert_data(self, url, data):
-        if type(data) in self.data_types:
+        if type(data) in self._data_types:
             return data
         else:
+            if not data:
+                return False
             data = data.rstrip().lstrip()
             # Replace escaped tags
             if self.parse_escaped:
@@ -156,11 +212,13 @@ class HTMLParser(BaseParser):
                 return False
 
             except Exception as e:
-                self.logger.exception("Unable to convert" + url + str(data))
+                self.logger.exception("Unable to convert" + url + str(data) +
+                                      str(type(data)))
                 return False
 
             urlparsed = urlparse.urlparse(url)
-            data.make_links_absolute(urlparsed.scheme + '://' + urlparsed.netloc)
+            data.make_links_absolute(urlparsed.scheme + '://' +
+                                     urlparsed.netloc)
             return data
 
     def _get_selector(self, selector):
@@ -168,7 +226,8 @@ class HTMLParser(BaseParser):
             if type(selector) in self.selectors:
                 return selector
             else:
-                assert type(selector) is str, "selector is not a string %r" %selector
+                assert type(selector) is str, \
+                    "selector is not a string %r" % selector
                 try:
                     return CSSSelector(selector)
                 except SelectorSyntaxError:
@@ -197,9 +256,22 @@ class HTMLParser(BaseParser):
                 yield objct
         return []
 
+    @format_docstring(selector_doc=_HTMLParser_selector_doc)
+    @add_other_doc(BaseParser._modify_text)
+    def text(self, selector: str=None, all_text: bool=True, **kwargs):
+        ''' Selects the text found between the HTML tags.
 
-    @add_other_doc(BaseParser._text)
-    def text(self, selector=None, all_text=True, **kwargs):
+        Parameters
+        ----------
+        selector : str, JavaScriptSelector, CSSOrSelector, optional
+                   {selector_doc}
+
+        all_text : bool, optional
+                   If set to True, all text in child elements will be
+                   selected too. Set it to False if only the text in the
+                   selected element should be parsed.
+        '''
+
         selector = self._get_selector(selector)
         return partial(self._text, selector=selector, all_text=True, **kwargs)
 
@@ -227,7 +299,7 @@ class HTMLParser(BaseParser):
                 if cells:
                     yield [cell.text_content() for cell in cells]
 
-    #@add_other_doc(BaseParser._modify_text)
+    @add_other_doc(BaseParser._modify_text)
     def attr(self, selector=None, attr: str='', **kwargs):
         ''' Extract an attribute of an HTML element.
 
@@ -244,18 +316,27 @@ class HTMLParser(BaseParser):
                        **kwargs)
 
     def _attr(self, url, data, selector=None, attr='', **kwargs):
-        elements = self._select(url, data, selector)
         for element in self._select(url, data, selector):
             sel_attr = element.attrib.get(attr)
-            yield self._modify_text(sel_attr, **kwargs)
+            if sel_attr:
+                yield self._modify_text(sel_attr, **kwargs)
 
-    #@add_other_doc(BaseParser._modify_text)
+    @format_docstring(selector_doc=_HTMLParser_selector_doc)
+    @add_other_doc(BaseParser._modify_text)
     def url(self, selector=None, **kwargs):
+        '''
+        Extracts the URL from the "href" attribute of the selected "a" tag.
+
+        Parameters
+        ----------
+        selector : str, optional
+                   {selector_doc}
+        '''# .format(selector_doc=self._selector_doc)
         selector = self._get_selector(selector)
         return partial(self._attr, selector=selector, attr='href', **kwargs)
 
     def date(self, selector=None, fmt: str='YYYYmmdd', attr: str=None, index:
-                 int=None):
+             int=None):
         '''
         Returns a python date object with the specified format.
         '''
@@ -266,9 +347,9 @@ class HTMLParser(BaseParser):
     def _date(self, url, data, selector=None, fmt='', attr=None, index=None):
         for element in self._select(url, data, selector):
             if attr:
-                date = sel_attr(element, selector, attr=attr, index=index)
+                date = self.attr(element, selector, attr=attr, index=index)
             else:
-                date = sel_text(element, selector, index=index)
+                date = self.text(element, selector, index=index)
             if date:
                 yield datetime.strptime(date, fmt)
 
@@ -329,34 +410,78 @@ class HTMLParser(BaseParser):
             self.parent._add_source(source)
 
 
-class JSONParser(HTMLParser):
-    name = 'JSONParser'
-    dialects = {key.lower(): val for key, val in sorted(vars(xmljson).items())
-                if isinstance(val, type) and issubclass(val, xmljson.XMLData)}
+class JSONParser(BaseParser):
+    '''
+    A parser that will parse JSON data. The JSON is converted to XML with the
+    xmljson package. This way, normal CSSSelectors, XPath selectors and the
+    other selectors that can be used to select data from the JSON.
 
+    .. xmljson
+
+    '''
+    name = 'JSONParser'
+    _data_types = [list, dict]
+    # _dialects = {key.lower(): val for key, val in sorted(vars(xmljson).items())
+    #             if isinstance(val, type) and issubclass(val, xmljson.XMLData)}
+    selectors = (_Program,)
+
+    # @add_other_doc(HTMLParser.__init__, section='Parameters')
     def __init__(self, dialect='badgerfish', invalid_tags='drop', *args,
                  **kwargs):
-        super().__init__(*args, **kwargs)
-        assert dialect in self.dialects, "Please choose a supported dialect" + \
-            str(dialects.keys())
-        self.converter = self.dialects[dialect](invalid_tags=invalid_tags)
+        '''
+        Parameters
+        ----------
+        dialect : str, optional
+                  The dialect to use in the conversion from JSON to XML. See
+                  the XMLJSON documentation for more information.
+
+        invalid_tags : str, optional
+                  Decide what to do with key that are valid in JSON but contain
+                  characters that are invalid according to the XML standard.
+                  The default is to drop these characters because the
+                  lxml.etree parsers raises a ValueError when it finds these
+                  invalid keys.
+        '''
+        super().__init__()
+        # assert dialect in self._dialects, "Please use a supported dialect" + \
+        #    str(self._dialects.keys())
+        # .self.converter = self._dialects[dialect](invalid_tags=invalid_tags)
 
     def _convert_data(self, url, data):
-        if data and type(data) in self.data_types:
+        if data and type(data) in self._data_types:
             return data
         elif data:
             try:
-                loaded_json = json.loads(data)
+                data = json.loads(data)
+                return data
             except Exception as E:
-                self.logger.exception(url + ' could not be loaded as json' + '\n' +
-                                 str(data))
+                self.logger.exception(url + ' could not be loaded as json' +
+                                      '\n' + str(data))
                 return False
-            xml = self.converter.etree(loaded_json, root=etree.Element('root'))
-            return xml
         else:
-            self.logger.warning(url + 'returned no data, perhaps the selector is '+
-                           'not working...')
+            self.logger.warning(url +
+                                'returned no data, perhaps the selector is ' +
+                                'not working...')
             return False
+
+    def _get_selector(self, selector):
+        if selector:
+            if type(selector) in self.selectors:
+                return selector
+            else:
+                assert type(selector) is str, "Selector must be str"
+                try:
+                    return jq(selector)
+                except:
+                    self.logger.exception(str(selector) + 'cannot compile')
+
+    def _select(self, url, data, selector, debug=False):
+        data = self._convert_data(url, data)
+        if data:
+            if selector:
+                return selector.transform(data, multiple_output=True)
+            return [data]
+        return []
 
     def dict(self, selector=None):
         selector = self._get_selector(selector)
